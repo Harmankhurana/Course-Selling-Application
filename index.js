38b00171b401231dbf9f8d67d3fdac9b53ba5a2e
Harman khurana <work.harmankhurana@gmail.com>
2026-03-01 20:57:04 +0530

    feat(index.js): adding userRouter, adminRouter and courseRouter

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const MONGO_URL = process.env.MONGO_URL;
 app.use(express.json());
 const app = express();
 
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/admin', adminRouter);
+app.use('/api/v1/course', courseRouter);
+
 async function connectionFirst()  {
     await mongoose.connect(MONGO_URL);
     console.log(`MoogoDB is connected with the server`);
