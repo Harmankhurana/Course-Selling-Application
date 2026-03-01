a5ddcd19e44ac610889be275bfe2a504a56cf5cd
Harman khurana <work.harmankhurana@gmail.com>
2026-03-02 02:55:54 +0530

    Working on admin.js endpoints

---
+++ routes/admin.js
@@ -0,0 +1,56 @@
+import jwt from 'jsonwebtoken';
+import bcrypt, { hash } from 'bcrypt';
+import { z } from 'zod';
+import Router from 'express';
+import { JWT_ADMIN_PASSWORD } from '../config.js';
+import { adminModel } from '../database/db.js';
+
+const adminRouter = Router();
+const saltRounds = 10;
+
+adminRouter.post('signup', async function (req, res) {
+    const requiredBody = z.object({
+        firstName: z.string,
+        lastName: z.string,
+        email: z.string,
+        password: z.string,
+    });
+
+    const parseDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parseDataWithSuccess.success) {
+        res.json({
+            message: "Incorrect format used",
+            error: parseDataWithSuccess,
+        });
+    };
+
+    const { firstName, lastName, email, password } = req.body;
+
+    try {
+        const hashedPassword = bcrypt.hash(password, saltRounds);
+        console.log(hashedPassword);
+
+        await adminModel.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: hashedPassword,
+        });
+
+        res.json({
+            message: "You're signed up in admin page",
+        });
+    } catch (e) {
+        res.json({
+            message: "Something went wrong while signing up",
+            error: e,
+        });
+    };
+});
+
+
+
+export {
+    adminRouter,
+}
