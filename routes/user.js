4cd81da100a0fa2e1e3f3aa7d0a0047adb83ee58
Harman khurana <work.harmankhurana@gmail.com>
2026-03-01 21:23:01 +0530

    Working on user.js file for writing endpoints

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,47 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { z } from 'zod';
 import Router from 'express';
+import { userModel } from '../database/db';
 
 const userRouter = Router();
+const saltRounds = 10;
 
 userRouter.post('/signup', async function(req, res) {
-    
+    requiredBody = z.object({
+        firstName: z.string(),
+        lastName: z.string(),
+        email: z.string(),
+        password: z.string(),
+    });
+
+    const parseDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parseDataWithSuccess.success) {
+        res.json({
+            message: "Incorrect format used",
+            error: parseDataWithSuccess,
+        });
+    }
+
+    const { firstName, lastName, email, password } = req.body;
+
+    try {
+        const hashedPasswor = bcrypt.hash(password, saltRounds);
+        console.log(hashedPasswor);
+
+        await userModel.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: hashedPasswor
+        });
+
+        res.json({
+            message: "You're signed up in User page"
+        })
+    } catch (e) {
+
+    }
 });
 
 userRouter.post('/signin', async function(req, res) {
