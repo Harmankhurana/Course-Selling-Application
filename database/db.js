c7b039a1c5c61b694a8524533c8416f82cbdfb73
Harman khurana <work.harmankhurana@gmail.com>
2026-03-02 02:42:11 +0530

    Working on db.js schema model

--- database/db.js
+++ database/db.js
@@ -1,47 +1,57 @@
 import mongoose from "mongoose";
-import { string } from "zod";
 
-const Schema = mongoose;
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    firstName: string,
-    lastName: string,
     email: {
+        type: String,
+        unique: true,
         required: true,
-        type: string,
     },
-    password: string
+    password: String,
+    firstName: String,
+    lastName: String,
 });
 
 const adminSchema = new Schema({
-    firstName: string,
-    lastName: string,
     email: {
-        required: true,
-        type: string,
+        type: String,
+        unique: true,
     },
-    password: string
+    password: String,
+    firstName: String,
+    lastName: String,
 });
+
 
 const courseSchema = new Schema({
-    title: string,
-    description: string,
-    price: string,
-    imageURL: string,
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
     creatorId: {
         type: Schema.Types.ObjectId,
-        ref: adminId,
-    }
+        ref: "admin"
+    },
 });
+
 
 const purchaseSchema = new Schema({
-
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+    },    
+    courseId: {
+        type: Schema.Types.ObjectId,
+        ref: "course",
+    },
 });
 
+
 const userModel = mongoose.model('user', userSchema);
-const adminModel = mongoose.model('user', adminSchema);
-const courseModel = mongoose.model('user', courseSchema);
-const purshaseModel = mongoose.model('user', purchaseSchema);
+const adminModel = mongoose.model('admin', adminSchema);
+const courseModel = mongoose.model('course', courseSchema);
+const purshaseModel = mongoose.model('purchase', purchaseSchema);
 
 export {
     userModel,
