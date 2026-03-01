757796b59d8a6491a2c404f4a6a07edf4d5fbb31
Harman khurana <work.harmankhurana@gmail.com>
2026-03-01 20:31:38 +0530

    config: add JWT secrets for admin and user via dotenv

---
+++ config.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
+
+export {
+    JWT_ADMIN_PASSWORD,
+    JWT_USER_PASSWORD
+}
